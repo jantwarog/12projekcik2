b9abb375bd0407b9d1a03184e3c3a0dbd002dd58
jantwarog <jasiu.tfaruk@gmail.com>
2019-06-22 11:00:28 +0200

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { BlogComponent } from './blog/blog.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactComponent,
+    AboutComponent,
+    GalleryComponent,
+    BlogComponent,
+    HomeComponent,
+
+  ],
+  imports: [
+    BrowserModule, NgbModule,
+    AppRoutingModule,
+    CarouselModule.forRoot(),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
